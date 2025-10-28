5f160a70bf20823964418a056c3cedddeb9c826c
JacobInocencio <80863831+JacobInocencio@users.noreply.github.com>
2025-10-28 08:04:31 -1000

    Create codingBat1.java

---
+++ CodingBatsMain/codingBat1.java
@@ -0,0 +1,129 @@
+//cigarParty
+
+public boolean cigarParty(int cigars, boolean isWeekend) {
+  
+  if (isWeekend && cigars>40) {
+    
+    return true;
+    
+  }
+
+    
+  else if(39<cigars && cigars<61) {
+    
+    return true;
+    
+  }
+    
+  else {
+    
+    return false;
+    
+  }
+  
+}
+
+
+//dateFashon
+
+public int dateFashion(int you, int date) {
+  
+  if ((you >= 8 || date >= 8) && (you>2 && date>2)) {
+
+      return 2;
+    
+  } 
+  
+  else if (you <= 2 || date <= 2) {
+    
+      return 0;
+    
+  }
+    
+  else {
+    
+    return 1;
+    
+  }
+  
+}
+
+
+//squirrlPlay
+
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  
+  if (temp>=60 && isSummer && temp <=100) {
+    
+    return true;
+    
+  }  
+     
+  else if (temp>=60 && temp <=90) {
+    
+    return true;
+    
+  }
+    
+  else {
+    
+    return false;
+    
+  }
+  
+}
+
+
+//caughtSpeeding
+
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  
+    if (isBirthday) {
+      speed -= 5;
+      
+    }
+  
+    if (speed<=60) {
+      
+      return 0;
+      
+    }
+      
+  else if (speed >= 61  && speed <=80) {
+    
+    return 1;
+    
+  }
+    
+  else if (speed >= 81) {
+    
+    return 2; 
+    
+  }
+    
+  else {
+    
+    return 0;
+    
+  }
+  
+}
+
+
+//sortaSum
+
+public int sortaSum(int a, int b) {
+  
+  if (((a+b) >= 20) || ((a+b)<=9) ) {
+    
+    return a+b;
+    
+  }
+    
+  else {
+    
+    return 20;
+    
+  }
+  
+}
