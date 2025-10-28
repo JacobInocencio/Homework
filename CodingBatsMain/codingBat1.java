56637775c58eff60c8b72807d98224c43681187d
JacobInocencio <80863831+JacobInocencio@users.noreply.github.com>
2025-10-28 08:20:05 -1000

    Update codingBat1.java

--- CodingBatsMain/codingBat1.java
+++ CodingBatsMain/codingBat1.java
@@ -28,23 +28,23 @@ public boolean cigarParty(int cigars, boolean isWeekend) {
 
 public int dateFashion(int you, int date) {
   
-  if ((you >= 8 || date >= 8) && (you>2 && date>2)) {
+    if ((you >= 8 || date >= 8) && (you>2 && date>2)) {
 
       return 2;
     
-  } 
+    } 
   
-  else if (you <= 2 || date <= 2) {
+    else if (you <= 2 || date <= 2) {
     
       return 0;
     
-  }
+    }
     
-  else {
+    else {
     
-    return 1;
+      return 1;
     
-  }
+    }
   
 }
 
@@ -53,23 +53,23 @@ public int dateFashion(int you, int date) {
 
 public boolean squirrelPlay(int temp, boolean isSummer) {
   
-  if (temp>=60 && isSummer && temp <=100) {
+   if (temp>=60 && isSummer && temp <=100) {
     
-    return true;
+     return true;
     
-  }  
+   }  
      
-  else if (temp>=60 && temp <=90) {
+   else if (temp>=60 && temp <=90) {
     
-    return true;
+     return true;
     
-  }
+   }
     
-  else {
+   else {
+     
+     return false;
     
-    return false;
-    
-  }
+   }
   
 }
 
@@ -79,7 +79,8 @@ public boolean squirrelPlay(int temp, boolean isSummer) {
 public int caughtSpeeding(int speed, boolean isBirthday) {
   
     if (isBirthday) {
-      speed -= 5;
+      
+     speed -= 5;
       
     }
   
@@ -89,23 +90,23 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
       
     }
       
-  else if (speed >= 61  && speed <=80) {
+    else if (speed >= 61  && speed <=80) {
     
-    return 1;
+       return 1;
     
-  }
+    }
     
-  else if (speed >= 81) {
+    else if (speed >= 81) {
     
-    return 2; 
+       return 2; 
     
-  }
+    }
     
-  else {
+    else {
     
-    return 0;
+       return 0;
     
-  }
+    }
   
 }
 
@@ -114,16 +115,16 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
 
 public int sortaSum(int a, int b) {
   
-  if (((a+b) >= 20) || ((a+b)<=9) ) {
+     if (((a+b) >= 20) || ((a+b)<=9) ) {
     
-    return a+b;
+        return a+b;
     
-  }
+     }
     
-  else {
+     else {
     
-    return 20;
+        return 20;
     
-  }
+     }
   
 }
